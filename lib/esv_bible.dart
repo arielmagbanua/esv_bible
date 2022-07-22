dc3107c026c7482ee8af581e3fd4ec8529f8ffc8
magbanua.c.ariel@gmail.com <magbanua.c.ariel@gmail.com>
2022-07-22 17:31:09 +0800

    Added factory use case.

--- lib/esv_bible.dart
+++ lib/esv_bible.dart
@@ -1,21 +1,43 @@
 library esv_bible;
 
 import 'package:http/http.dart' as http;
-import 'package:esv_bible/data/data_sources/esv_remote_api_data_source.dart';
-import 'package:esv_bible/data/repositories/esv_bible_repository.dart';
-import 'package:esv_bible/domain/use_cases/passage_text.dart';
+import 'data/data_sources/esv_remote_api_data_source.dart';
+import 'data/repositories/esv_bible_repository.dart';
+import 'domain/use_cases/passage_text.dart';
+import 'domain/use_cases/passage_html.dart';
+import 'domain/use_cases/passage_search.dart';
 
 abstract class GetPassage {
   const GetPassage();
 
-  factory GetPassage.text() {
+  factory GetPassage.text(String key) {
     final dataSource = EsvRemoteDataSourceImplementation(
       httpClient: http.Client(),
-      apiKey: 'adf6689e8a9a0ad80364ed1a1cb2f30a682f46fd',
+      apiKey: key,
     );
     final esvBibleRepository = EsvBibleRepository(dataSource);
 
     return GetPassageText(esvBibleRepository);
+  }
+
+  factory GetPassage.html(String key) {
+    final dataSource = EsvRemoteDataSourceImplementation(
+      httpClient: http.Client(),
+      apiKey: key,
+    );
+    final esvBibleRepository = EsvBibleRepository(dataSource);
+
+    return GetPassageHtml(esvBibleRepository);
+  }
+
+  factory GetPassage.search(String key) {
+    final dataSource = EsvRemoteDataSourceImplementation(
+      httpClient: http.Client(),
+      apiKey: key,
+    );
+    final esvBibleRepository = EsvBibleRepository(dataSource);
+
+    return GetPassageSearch(esvBibleRepository);
   }
 
   Future<dynamic> call(
@@ -23,11 +45,4 @@ abstract class GetPassage {
     Map<String, dynamic>? params,
     Map<String, dynamic>? headers,
   });
-}
-
-void main() async {
-  final getPassageText = GetPassage.text();
-  final data = await getPassageText('John 3:16');
-
-  print(data);
 }
