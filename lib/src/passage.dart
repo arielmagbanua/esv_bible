63d1fd2adb09ccd31603eb805b36e5922c3f9972
magbanua.c.ariel@gmail.com <magbanua.c.ariel@gmail.com>
2022-07-29 15:10:45 +0800

    Implemented a new Passage class.

---
+++ lib/src/passage.dart
@@ -0,0 +1,61 @@
+import 'package:http/http.dart' as http;
+
+import 'data/data_sources/esv_remote_api_data_source.dart';
+import 'data/repositories/esv_bible_repository.dart';
+import 'domain/entities/passage_html.dart';
+import 'domain/entities/passage_search.dart';
+import 'domain/entities/passage_text.dart';
+
+/// Passage
+///
+/// The class for retrieving passage data from ESV API.
+class Passage {
+  final String apiKey;
+  final EsvBibleRepository esvBibleRepository;
+
+  Passage({
+    required this.apiKey,
+    EsvBibleRepository? esvBibleRepository,
+  }) : esvBibleRepository = esvBibleRepository ??
+            EsvBibleRepository(EsvRemoteDataSourceImplementation(
+              httpClient: http.Client(),
+              apiKey: apiKey,
+            ));
+
+  /// Retrieves a passage text data from ESV API.
+  ///
+  /// The [queryPassage] is the requested passage.
+  /// The [params] is a key value map as url params.
+  /// The [headers] is a map that contains the headers of the request.
+  Future<PassageText> text(
+    String queryPassage, {
+    Map<String, dynamic>? params,
+    Map<String, dynamic>? headers,
+  }) {
+    return esvBibleRepository.getPassageText(queryPassage);
+  }
+
+  /// Retrieves a passage html data from ESV API.
+  ///
+  /// The [queryPassage] is the requested passage.
+  /// The [params] is a key value map as url params.
+  /// The [headers] is a map that contains the headers of the request.
+  Future<PassageHtml> html(String queryPassage, {
+    Map<String, dynamic>? params,
+    Map<String, dynamic>? headers,
+  }) {
+    return esvBibleRepository.getPassageHtml(queryPassage);
+  }
+
+  /// Retrieves a passage search data from ESV API.
+  ///
+  /// The [queryPassage] is the requested passage.
+  /// The [params] is a key value map as url params.
+  /// The [headers] is a map that contains the headers of the request.
+  Future<PassageSearch> search(String queryPassage, {
+    Map<String, dynamic>? params,
+    Map<String, dynamic>? headers,
+  }) {
+    return esvBibleRepository.getPassageSearch(queryPassage);
+  }
+}
