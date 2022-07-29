23f731fb642de08d21d818b8f8d33811eabf2d78
magbanua.c.ariel@gmail.com <magbanua.c.ariel@gmail.com>
2022-07-29 15:14:03 +0800

    Updated the example.

--- example/main.dart
+++ example/main.dart
@@ -3,7 +3,9 @@ import 'package:esv_bible/esv_bible.dart';
 void main () async {
   const apiKey = 'your-api-key';
 
-  final passage = GetPassage.text(apiKey);
-  final passageText = await passage('John 3:16');
+  final passage = Passage(apiKey: apiKey);
+
+  // PassageText
+  final passageText = await passage.text('John 3:16');
   print(passageText.passages[0]);
 }
