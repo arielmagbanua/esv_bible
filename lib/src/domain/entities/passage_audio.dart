1a0c60ab4048a82bc9df701743093a93bda62b4d
magbanua.c.ariel@gmail.com <magbanua.c.ariel@gmail.com>
2022-08-01 20:22:06 +0800

    Added PassageAudio class.

---
+++ lib/src/domain/entities/passage_audio.dart
@@ -0,0 +1,24 @@
+import 'dart:io';
+
+import 'package:equatable/equatable.dart';
+
+/// PassageAudio
+///
+/// The entity class for PassageAudio data response that
+/// contains an audio file.
+class PassageAudio extends Equatable {
+  final String query;
+
+  final File audio;
+
+  PassageAudio({
+    required this.query,
+    required this.audio,
+  });
+
+  @override
+  List<Object?> get props => [
+        query,
+        audio,
+      ];
+}
