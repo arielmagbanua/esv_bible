832978617baac9a0811550eb45b7d70a0e6dbb96
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2022-08-22 21:50:21 +0800

    1.1.2 (#22)
    * Renamed.
    
    * Exported needed classes.
    
    * Organized imports.
    
    * organized imports.
    
    * Cleaned up
    
    * Refactored and improved the data source.
    
    * Changed the version.
    
    * Updated tests.
    
    * Extracted the implementation to separate class.
    
    * Updated changelog.
    
    * Fixed the format.
    
    * Added more documentation.
    
    * Improved the exporting.
    
    * Updated the version and the changelog.
    
    * Corrected the class usages.
    
    * Updated doc strings.
    
    * Updated CI steps.
    
    * Ignored the pubspec.lock.
    
    * Updated the version and change log.
    
    * Fixed github action and updated packages.

--- lib/src/passage.dart
+++ lib/src/passage.dart
@@ -1,7 +1,8 @@
 import 'package:http/http.dart' as http;
 
 import 'data/data_sources/esv_remote_data_source_implementation.dart';
-import 'data/repositories/esv_bible_repository.dart';
+import 'data/repositories/esv_bible_repository.dart' as implementations;
+import 'domain/repositories/esv_bible_repository.dart' as contracts;
 import 'domain/entities/passage_audio.dart';
 import 'domain/entities/passage_html.dart';
 import 'domain/entities/passage_search.dart';
@@ -11,14 +12,17 @@ import 'domain/entities/passage_text.dart';
 ///
 /// The class for retrieving passage data from ESV API.
 class Passage {
+  /// The api key
   final String apiKey;
-  final EsvBibleRepository esvBibleRepository;
+
+  /// The ESV bible repository instance
+  final contracts.EsvBibleRepository esvBibleRepository;
 
   Passage(
     this.apiKey, {
-    EsvBibleRepository? esvBibleRepository,
+    contracts.EsvBibleRepository? esvBibleRepository,
   }) : esvBibleRepository = esvBibleRepository ??
-            EsvBibleRepository(
+            implementations.EsvBibleRepository(
               EsvRemoteDataSourceImplementation(
                 httpClient: http.Client(),
                 apiKey: apiKey,
